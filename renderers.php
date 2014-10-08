8a8d43ebac11a97200b55eac88692e70d6c701dd
Will <will@arkade.com.au>
2014-10-08 17:20:45 +1100

    done

--- renderers.php
+++ renderers.php
@@ -1,53 +1,53 @@
-<?php
+// <?php
 
-class theme_custom1_core_renderer extends core_renderer {
-    /**
-     * Produces a header for a block
-     *
-     * @param block_contents $bc
-     * @return string
-     */
-    protected function block_header(block_contents $bc) {
+// class theme_custom1_core_renderer extends core_renderer {
+//     /**
+//      * Produces a header for a block
+//      *
+//      * @param block_contents $bc
+//      * @return string
+//      */
+//     protected function block_header(block_contents $bc) {
 
-        $title = '';
-        if ($bc->title) {
-            $attributes = array();
-            if ($bc->blockinstanceid) {
-                $attributes['id'] = 'instance-'.$bc->blockinstanceid.'-header';
-            }
-            $title = html_writer::tag('h2', $bc->title, $attributes);
-        }
+//         $title = '';
+//         if ($bc->title) {
+//             $attributes = array();
+//             if ($bc->blockinstanceid) {
+//                 $attributes['id'] = 'instance-'.$bc->blockinstanceid.'-header';
+//             }
+//             $title = html_writer::tag('h2', $bc->title, $attributes);
+//         }
 
-        $blockid = null;
-        if (isset($bc->attributes['id'])) {
-            $blockid = $bc->attributes['id'];
-        }
-        $controlshtml = $this->block_controls($bc->controls, $blockid);
+//         $blockid = null;
+//         if (isset($bc->attributes['id'])) {
+//             $blockid = $bc->attributes['id'];
+//         }
+//         $controlshtml = $this->block_controls($bc->controls, $blockid);
 
-        $output = '';
-        if ($title || $controlshtml) {
-            $output .= html_writer::tag('div', html_writer::tag('div', html_writer::tag('div', '', array('class'=>'block_action')). $title . $controlshtml, array('class' => 'title')), array('class' => 'header'));
-        }
-        return $output;
-    }
+//         $output = '';
+//         if ($title || $controlshtml) {
+//             $output .= html_writer::tag('div', html_writer::tag('div', html_writer::tag('div', '', array('class'=>'block_action')). $title . $controlshtml, array('class' => 'title')), array('class' => 'header'));
+//         }
+//         return $output;
+//     }
 
-    /**
-     * Produces the content area for a block
-     *
-     * @param block_contents $bc
-     * @return string
-     */
-    protected function block_content(block_contents $bc) {
-        $output = html_writer::start_tag('div', array('class' => 'content'));
-        if (!$bc->title && !$this->block_controls($bc->controls)) {
-            $output .= html_writer::tag('div', '', array('class'=>'block_action notitle'));
-        }
-        $output .= $bc->content;
-        $output .= $this->block_footer($bc);
-        $output .= html_writer::end_tag('div');
+//     /**
+//      * Produces the content area for a block
+//      *
+//      * @param block_contents $bc
+//      * @return string
+//      */
+//     protected function block_content(block_contents $bc) {
+//         $output = html_writer::start_tag('div', array('class' => 'content'));
+//         if (!$bc->title && !$this->block_controls($bc->controls)) {
+//             $output .= html_writer::tag('div', '', array('class'=>'block_action notitle'));
+//         }
+//         $output .= $bc->content;
+//         $output .= $this->block_footer($bc);
+//         $output .= html_writer::end_tag('div');
 
-        return $output;
-    }
-}
+//         return $output;
+//     }
+// }
 
-?>
+// ?>
